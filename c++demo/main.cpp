99e040d17ca129bb4df1915375c939bb68a4722c
Mark Du <dulongjiebackup@gmail.com>
2024-05-03 13:48:09 -0400

    Initial Commit

---
+++ c++demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++demo
+//
+//  Created by Mark Du on 2024-05-03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
