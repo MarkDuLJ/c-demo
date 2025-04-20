98e270c495ff12b620785b23472d929b4ff5fb24
Mark Du <dulongjiebackup@gmail.com>
2025-04-20 08:51:15 -0700

    test

---
+++ c++demo/sort/sort.cpp
@@ -0,0 +1,8 @@
+//
+//  sort.cpp
+//  c++demo
+//
+//  Created by Mark Du on 2025-04-20.
+//
+
+#include "sort.hpp"
---
+++ c++demo/sort/sort.hpp
@@ -0,0 +1,13 @@
+//
+//  sort.hpp
+//  c++demo
+//
+//  Created by Mark Du on 2025-04-20.
+//
+
+#ifndef sort_hpp
+#define sort_hpp
+
+#include <stdio.h>
+
+#endif /* sort_hpp */
